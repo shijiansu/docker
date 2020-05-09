978f148c56da251f2c006a0f1c46f69b75aaed4f
Shijian Su <sushijian.iss.nus@gmail.com>
2020-05-10 01:38:54 +0800

    Add example of food trucks

---
+++ docker-for-beginners/5_3_food-trucks-all-in-one.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/bin/bash ./docker/food-trucks/setup-docker.sh
+
+# clean resource
+# docker container stop foodtrucks-web && docker container rm foodtrucks-web
+# docker container stop es && docker container rm es
+# docker network rm foodtrucks-net
+# docker image rm -f prakhar1989/foodtrucks-web
